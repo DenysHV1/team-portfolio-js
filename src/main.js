d2deb1270a132ef2f2ba66aa26dc6a36e056120f
DenVik <denyagarkusha2@gmail.com>
2024-10-10 00:01:43 +0200

    prittered

--- src/main.js
+++ src/main.js
@@ -1,15 +1,14 @@
-import { faq } from "./js/faq/faq";
-faq()
+import { faq } from './js/faq/faq';
+faq();
 
-import { header } from "./js/header/header";
-header()
+import { header } from './js/header/header';
+header();
 
-import { myProjects } from "./js/my-projects/my-projects";
-myProjects()
+import { myProjects } from './js/my-projects/my-projects';
+myProjects();
 
-import { reviews } from "./js/reviews/reviews";
-reviews()
+import { reviews } from './js/reviews/reviews';
+reviews();
 
-import { workTogether } from "./js/work-together/work-together";
-
-workTogether()
+import { workTogether } from './js/work-together/work-together';
+workTogether();
