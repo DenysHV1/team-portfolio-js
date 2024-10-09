3671e72a7bfb02070c133b6c276c9fdab9393c40
DenVik <denyagarkusha2@gmail.com>
2024-10-09 23:48:21 +0200

    start

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import { faq } from "./js/faq/faq";
+faq()
+
+import { header } from "./js/header/header";
+header()
+
+import { myProjects } from "./js/my-projects/my-projects";
+myProjects()
+
+import { reviews } from "./js/reviews/reviews";
+reviews()
+
+import { workTogether } from "./js/work-together/work-together";
+
+workTogether()
