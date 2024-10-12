d36e7745c54a2cbab84fa63c1d21c70b7412058c
aliso4ka <gulyayevaalisa@gmail.com>
2024-10-12 12:08:18 +0200

    f

--- src/js/reviews/render-reviews.js
+++ src/js/reviews/render-reviews.js
@@ -5,7 +5,7 @@ export default function createMarkup(arr = []) {
         `<li class="list-item swiper-slide">
           <p class="review-text">${item.review}</p>
           <div class="name-and-img">
-            <img src="${item.avatar_url}" alt="" class="review-img" />
+            <img src="${item.avatar_url}" alt="${item.author}" class="review-img" />
             <p class="review-name">${item.author}</p>
           </div>
         </li>`
--- src/js/reviews/reviews.js
+++ src/js/reviews/reviews.js
@@ -1,6 +1,6 @@
 import Swiper from 'swiper';
-import { Navigation, Keyboard, Mousewheel} from 'swiper/modules';
-import 'swiper/swiper-bundle.css'; 
+import { Navigation, Keyboard, Mousewheel } from 'swiper/modules';
+import 'swiper/swiper-bundle.css';
 
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
@@ -8,60 +8,56 @@ import 'izitoast/dist/css/iziToast.min.css';
 import getReview from './api-reviews';
 import createMarkup from './render-reviews';
 
-
-const reviewsList = document.querySelector(".list-reviews");
-
+const reviewsList = document.querySelector('.list-reviews');
 
 let swiper;
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', () => {
   swiper = new Swiper('.swiper', {
-   modules:[Navigation, Keyboard, Mousewheel],
+    modules: [Navigation, Keyboard, Mousewheel],
     navigation: {
       nextEl: '.swiper-button-next',
-     prevEl: '.swiper-button-prev',
-     disabledClass: "disabled",
+      prevEl: '.swiper-button-prev',
+      disabledClass: 'disabled',
     },
     slidesPerView: 1,
     spaceBetween: 20,
     keyboard: {
       enabled: true,
-   }, 
+    },
     mousewheel: {
       enabled: true,
       sensitivity: 2,
     },
-    speed: 200,
- 
+    speed: 350,
+
     breakpoints: {
       1280: {
-       slidesPerView: 'auto',
+        slidesPerView: 'auto',
         spaceBetween: 32,
-     
-      }
+      },
     },
-    
   });
 });
 
-document.addEventListener('keydown', (event) => {
+document.addEventListener('keydown', event => {
   if (event.key === 'Tab') {
-    event.preventDefault(); 
+    event.preventDefault();
 
     const activeIndex = swiper.activeIndex;
     const isShiftPressed = event.shiftKey;
 
-    
     if (isShiftPressed) {
-      
-      swiper.slideTo(activeIndex > 0 ? activeIndex - 1 : swiper.slides.length - 1);
+      swiper.slideTo(
+        activeIndex > 0 ? activeIndex - 1 : swiper.slides.length - 1
+      );
     } else {
-      
-      swiper.slideTo(activeIndex < swiper.slides.length - 1 ? activeIndex + 1 : 0);
+      swiper.slideTo(
+        activeIndex < swiper.slides.length - 1 ? activeIndex + 1 : 0
+      );
     }
   }
 });
-
 
 export async function reviews() {
   console.log('Loading reviews...');
@@ -70,40 +66,35 @@ export async function reviews() {
     const response = await getReview();
 
     if (response) {
-   
       reviewsList.innerHTML = createMarkup(response);
- 
-      swiper.update(); 
-    
-    } else {
 
-        iziToast.error({
+      swiper.update();
+    } else {
+      iziToast.error({
         title: 'Error',
         message: 'No reviews found',
-        position: 'topRight', 
-        timeout: 5000, 
-        backgroundColor: '#FF4D4D', 
-        color: '#FFFFFF', 
-        zindex: 9999, 
-        });
-      
+        position: 'topRight',
+        timeout: 5000,
+        backgroundColor: '#FF4D4D',
+        color: '#FFFFFF',
+        zindex: 9999,
+      });
+
       reviewsList.innerHTML = '<p class="notFound">Not found</p>';
     }
   } catch (error) {
     console.error(error);
 
     iziToast.error({
-        title: 'Error',
-        message: 'No reviews found',
-        position: 'topRight',
-        timeout: 5000, 
-        backgroundColor: '#FF4D4D', 
-        color: '#FFFFFF', 
-        zindex: 9999, 
+      title: 'Error',
+      message: 'No reviews found',
+      position: 'topRight',
+      timeout: 5000,
+      backgroundColor: '#FF4D4D',
+      color: '#FFFFFF',
+      zindex: 9999,
     });
-    
+
     reviewsList.innerHTML = '<p class="notFound">Not found</p>';
   }
 }
-
-
