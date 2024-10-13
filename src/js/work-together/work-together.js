0100890a6ae79c352aa5bb40501980c66ce27ff7
Sairus78 <83472347+Sairus78@users.noreply.github.com>
2024-10-13 22:48:00 +0300

    fin

--- src/js/work-together/work-together.js
+++ src/js/work-together/work-together.js
@@ -1,4 +1,4 @@
-//? ця функція не приймає ніяких аргументів і вона потрібна для того щоб ніхто не чепав main.js 
+//? ця функція не приймає ніяких аргументів і вона потрібна для того щоб ніхто не чепав main.js
 //? функція потрібна як обгортка для всієї логіки
 //? вся логіка знаходиться всередині
 //? ви маєте папку (для своїх JS файлів) в ній, при необхідності, можете створювати інші JS файли (залежно від завдання) і виклик тих функцій повинен бути в основній функції, тобто вы можете написати всередині цієї функції безліч інших функцій, або їх виклики
@@ -7,8 +7,13 @@
 //your imports is here (if you have)
 
 //
+import { setupEmailValidation } from '../work-together/validation';
 
-export function workTogether(){
-	console.log('hello world from workTogether');
-	// your code is here ⏬'
+export function workTogether() {
+  console.log('hello world from workTogether');
+  // your code is here ⏬'
+  const emailPattern = /^\w+(\.\w+)?@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+
+  // Викликаємо валідацію для форми
+  setupEmailValidation('.form-list', '.form-validation-text', emailPattern);
 }
